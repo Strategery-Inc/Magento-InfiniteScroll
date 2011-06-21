5696fd028bfdbd77a857368bb834873bf9f42e6f
gabrielsomoza <work@gabrielsomoza.com>
2011-06-21 08:27:06 +0200

    Some JS and selector tweaks, now it works 100% out of the box with Magento 1.5.1.0; Renamed entire module Strategery_Infinitescroll; moved some files around for better directory structure.

--- infinitescroll/app/code/local/Strategery/Infinitescroll/Helper/Data.php
+++ infinitescroll/app/code/local/Strategery/Infinitescroll/Helper/Data.php
@@ -1,12 +1,12 @@
 <?php
-/*
+/**
  * @author     Gabriel Somoza (me@gabrielsomoza.com)
- * @url        http://gabrielsomoza.com/
- * @category   Local
- * @package    Gabrielsomoza_Infinitescroll
+ * @link       http://gabrielsomoza.com/
+ * @category   Strategery
+ * @package    Strategery_Infinitescroll	   
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Gabrielsomoza_Infinitescroll_Helper_Data extends Mage_Core_Helper_Abstract {
+class Strategery_Infinitescroll_Helper_Data extends Mage_Core_Helper_Abstract {
     public function getConfigData($node) {
         return Mage::getStoreConfig('infinitescroll/' . $node);
     }
--- infinitescroll/app/code/local/Strategery/Infinitescroll/controllers/JsController.php
+++ infinitescroll/app/code/local/Strategery/Infinitescroll/controllers/JsController.php
@@ -1,12 +1,12 @@
 <?php
-/*
+/**
  * @author     Gabriel Somoza (me@gabrielsomoza.com)
- * @url        http://gabrielsomoza.com/
- * @category   Local
- * @package    Gabrielsomoza_Infinitescroll
+ * @link       http://gabrielsomoza.com/
+ * @category   Strategery
+ * @package    Strategery_Infinitescroll	   
  * @license    http://opensource.org/licenses/osl-3.0.php  Open Software License (OSL 3.0)
  */
-class Gabrielsomoza_Infinitescroll_JsController extends Mage_Core_Controller_Front_Action {
+class Strategery_Infinitescroll_JsController extends Mage_Core_Controller_Front_Action {
     public function indexAction() {
         $this->getResponse()->setHeader('Content-Type', 'text/javascript');
         $this->loadLayout();
