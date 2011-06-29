7aa4820213a7ec1bc2645cd6b86e60f96cee1bb1
Nicolas Far <nicolas.far@usestrategery.com>
2011-06-29 10:00:25 -0300

    * Added infinite-scroll 2.0 JS * Added Magento behaviour for infinite-scroll * Modified layout to include the new infinite-scroll and behaviour * Modified JS template to include behaviour * Added missing property to config.xml

---
+++ infinitescroll/js/jquery/behaviors/strategery-magento.js
@@ -0,0 +1,93 @@
+/*
+	--------------------------------
+	Infinite Scroll Behavior
+	Behavior for Magento
+	--------------------------------
+	+ https://github.com/paulirish/infinitescroll/
+	+ version 2.0b2.110617
+	+ Copyright 2011 Paul Irish & Luke Shumard
+	+ Licensed under the MIT license
+	
+	+ Documentation: http://infinite-scroll.com/
+	
+*/
+(function($){
+    $(document).ready(function(){
+        $.extend($.infinitescroll.prototype,{
+
+            _loadcallback_magento: function infscr_loadcallback_magento (box,data) {
+
+                var opts = this.options,
+	    		callback = this.options.callback, // GLOBAL OBJECT FOR CALLBACK
+	    		result = ( opts.wasDoneLastTime||opts.isDone) ? 'done' : (!opts.appendCallback) ? 'no-append' : 'append',
+	    		frag;
+                originalData=data;
+                switch (result) {
+
+                    case 'done':
+                        
+                        this.options.isDone=true;
+                        this._showdonemsg();
+                        return false;
+
+                        break;
+
+                    case 'no-append':
+
+                        if (opts.dataType == 'html') {
+                            data = '<div>' + data + '</div>';
+                            data = $(data).find(opts.itemSelector);
+                        };
+
+                        break;
+
+                    case 'append':
+
+                        var children = box.children();
+
+                        // if it didn't return anything
+                        if (children.length == 0) {
+                            return this._error('end');
+                        }
+
+
+                        // use a documentFragment because it works when content is going into a table or UL
+                        frag = document.createDocumentFragment();
+                        while (box[0].firstChild) {
+                            frag.appendChild(box[0].firstChild);
+                        }
+
+                        this._debug('contentSelector', $(opts.contentSelector)[0])
+                        $(opts.contentSelector)[0].appendChild(frag);
+                        // previously, we would pass in the new DOM element as context for the callback
+                        // however we're now using a documentfragment, which doesnt havent parents or children,
+                        // so the context is the contentContainer guy, and we pass in an array
+                        //   of the elements collected as the first argument.
+
+                        data = children.get();
+
+
+                        break;
+
+                }
+                // loadingEnd function
+                opts.loadingEnd.call($(opts.contentSelector)[0],opts)
+
+
+                // smooth scroll to ease in the new content
+                if (opts.animate) {
+                    var scrollTo = $(window).scrollTop() + $('#infscr-loading').height() + opts.extraScrollPx + 'px';
+                    $('html,body').animate({ scrollTop: scrollTo }, 800, function () { opts.isDuringAjax = false; });
+                }
+
+                if (!opts.animate) opts.isDuringAjax = false; // once the call is done, we can allow it again.
+                
+                if ($(this.options.nextSelector,originalData).length==0){ // fix for magento
+                    this.options.wasDoneLastTime=true;
+                }
+                callback.call($(opts.contentSelector)[0], data);
+            }
+
+        });
+    });
+})(jQuery);
