5a4f028c6583ce957a4c9902684e939a8f6cfe72
gabrielsomoza <work@gabrielsomoza.com>
2011-07-08 16:42:44 +0200

    Fixed behavior to adapt to latest changes in InfiniteScroll core.

--- infinitescroll/js/jquery/infinitescroll/behaviors/infinitescroll-magento.js
+++ infinitescroll/js/jquery/infinitescroll/behaviors/infinitescroll-magento.js
@@ -19,25 +19,21 @@ Behavior for Magento
         $.extend($.infinitescroll.prototype,{
             _loadcallback_magento: function infscr_loadcallback_magento (box,data) {
                 var opts = this.options,
-                callback = this.options.callback, // GLOBAL OBJECT FOR CALLBACK
-                result = ( opts.wasDoneLastTime || opts.isDone) ? 'done' : (!opts.appendCallback) ? 'no-append' : 'append',
-                originalData = data,
-                frag;
-                        
+                    callback = this.options.callback, // GLOBAL OBJECT FOR CALLBACK
+                    result = ( opts.wasDoneLastTime || opts.isDone) ? 'done' : (!opts.appendCallback) ? 'no-append' : 'append',
+                    originalData = data,
+                    frag;
                 switch (result) {
                     case 'done':
-                        this.options.isDone=true;
                         this._showdonemsg();
                         return false;
                         break;
-
                     case 'no-append':
                         if (opts.dataType == 'html') {
                             data = '<div>' + data + '</div>';
                             data = $(data).find(opts.itemSelector);
                         };
                         break;
-
                     case 'append':
                         var children = box.children();
                         if (children.length == 0) {
@@ -51,19 +47,19 @@ Behavior for Magento
                         $(opts.contentSelector)[0].appendChild(frag);
                         data = children.get();
                         break;
-
                 }
-                opts.loadingEnd.call($(opts.contentSelector)[0],opts)
+                opts.loading.finished.call($(opts.contentSelector)[0],opts);
                 if (opts.animate) {
                     var scrollTo = $(window).scrollTop() + $('#infscr-loading').height() + opts.extraScrollPx + 'px';
                     $('html,body').animate({
                         scrollTop: scrollTo
                     }, 800, function () {
-                        opts.isDuringAjax = false;
+                        opts.state.isDuringAjax = false;
                     });
                 }
-                if (!opts.animate) opts.isDuringAjax = false; // once the call is done, we can allow it again.
-                if ($(this.options.nextSelector,originalData).length==0){ // detect last page in Magento catalog
+                if (!opts.animate) opts.state.isDuringAjax = false;
+                // detect last page in Magento catalog
+                if ($(this.options.nextSelector,originalData).length==0){ 
                     this.options.wasDoneLastTime=true;
                 }
                 callback.call($(opts.contentSelector)[0], data);
