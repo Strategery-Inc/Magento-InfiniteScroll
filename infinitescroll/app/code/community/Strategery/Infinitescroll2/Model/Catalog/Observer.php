c9e82808447009f5b6edca28e61f035b661e08b9
Damian A. Pastorini <damian.pastorini@usestrategery.com>
2013-01-28 09:53:46 -0300

    [STRAI-479] InfiniteScroll improvements and callback function fix.

--- infinitescroll/app/code/community/Strategery/Infinitescroll2/Model/Catalog/Observer.php
+++ infinitescroll/app/code/community/Strategery/Infinitescroll2/Model/Catalog/Observer.php
@@ -21,117 +21,131 @@ class Strategery_Infinitescroll2_Model_Catalog_Observer
 	
 	public function modifyCollection($observer)
 	{
-		// reset:
-		$this->hardReset();
-		// helper:
-		$helper = Mage::helper('infinitescroll2');
-		// observer data:
-		$event = $observer->getEvent();
-        $cacheName = str_replace('/','_',Mage::app()->getRequest()->getRequestString());
-        if(Mage::registry('current_category'))
-        {
-            $cacheName = Mage::registry('current_category')->getId();
+        // check general and instance enable:
+	    $whereare = $this->_whereAreWe();
+	    if(Mage::getStoreConfig('infinitescroll2/general/enable') && Mage::getStoreConfig('infinitescroll2/instances/'.$whereare))
+	    {
+		    // reset:
+		    $this->hardReset();
+		    // helper:
+		    $helper = Mage::helper('infinitescroll2');
+		    // observer data:
+		    $event = $observer->getEvent();
+            $cacheName = str_replace('/','_',Mage::app()->getRequest()->getRequestString());
+            if(Mage::registry('current_category'))
+            {
+                $cacheName = Mage::registry('current_category')->getId();
+            }
+            $collection = $this->_getCache($observer, $cacheName);
+		    $lastPageNumber = $collection->getLastPageNumber();
+		    if(Mage::registry('current_category') && $helper->isMemoryActive() && $lastPageNumber>1)
+		    {
+			    // info:
+			    $pageId = Mage::registry('current_category')->getId();
+			    $pageByParam = $helper->getNextPageNumber();
+			    $pageLoaded = $helper->loadMemory($pageId);
+			    // chek page size or default
+			    if (Mage::getStoreConfig('infinitescroll2/instances/size_'.$whereare.''))
+        			$defaultPageSize = Mage::getStoreConfig('infinitescroll2/instances/size_'.$whereare.'');			
+			    else
+        			$defaultPageSize = $collection->getPageSize();
+        			
+			    Mage::getSingleton('checkout/session')->setData('defautlPageSize',$defaultPageSize);
+			    // actions:
+			    if(!$helper->isScrollCall())
+			    {
+				    if(!Mage::getSingleton('checkout/session')->getData('recursiveCollection'))
+				    {
+					    if($pageLoaded>1)
+					    {
+						    Mage::getSingleton('checkout/session')->setData('recursiveCollection',true);
+						    Mage::getSingleton('checkout/session')->setData('pageLoaded',$pageLoaded);
+						    // replace page size:
+						    $tmpPageSize = $defaultPageSize*$pageLoaded;
+						    $collection->setPageSize($tmpPageSize);
+					    }
+					    else
+					    {
+						    Mage::getSingleton('checkout/session')->setData('pageLoaded','');
+						    Mage::getSingleton('checkout/session')->setData('nextPage','');
+					    }
+				    }
+			    }
+			    else
+			    {
+				    $nextPage = Mage::getSingleton('checkout/session')->getData('nextPage');
+				    if($pageLoaded>$nextPage)
+				    {
+					    $nextPage = $pageLoaded+1;
+				    }
+				    if($nextPage>1 && $nextPage<=$lastPageNumber)
+				    {
+					    $pageByParam=$nextPage;
+				    }
+				    if($nextPage<=$lastPageNumber)
+				    {
+					    $helper->saveMemory($pageByParam,$pageId);
+				    }
+				    $collection->setCurPage($pageByParam);
+				    Mage::getSingleton('checkout/session')->setData('pageLoaded',$pageByParam);
+			    }
+		    }
+		    return $this;
         }
-        $collection = $this->_getCache($observer, $cacheName);
-		$lastPageNumber = $collection->getLastPageNumber();
-		if(Mage::registry('current_category') && $helper->isMemoryActive() && $lastPageNumber>1)
-		{
-			// info:
-			$pageId = Mage::registry('current_category')->getId();
-			$pageByParam = $helper->getNextPageNumber();
-			$pageLoaded = $helper->loadMemory($pageId);
-			// default page size save:
-			$defaultPageSize = $collection->getPageSize();
-			Mage::getSingleton('checkout/session')->setData('defautlPageSize',$defaultPageSize);
-			// actions:
-			if(!$helper->isScrollCall())
-			{
-				if(!Mage::getSingleton('checkout/session')->getData('recursiveCollection'))
-				{
-					if($pageLoaded>1)
-					{
-						Mage::getSingleton('checkout/session')->setData('recursiveCollection',true);
-						Mage::getSingleton('checkout/session')->setData('pageLoaded',$pageLoaded);
-						// replace page size:
-						$tmpPageSize = $defaultPageSize*$pageLoaded;
-						$collection->setPageSize($tmpPageSize);
-					}
-					else
-					{
-						Mage::getSingleton('checkout/session')->setData('pageLoaded','');
-						Mage::getSingleton('checkout/session')->setData('nextPage','');
-					}
-				}
-			}
-			else
-			{
-				$nextPage = Mage::getSingleton('checkout/session')->getData('nextPage');
-				if($pageLoaded>$nextPage)
-				{
-					$nextPage = $pageLoaded+1;
-				}
-				if($nextPage>1 && $nextPage<=$lastPageNumber)
-				{
-					$pageByParam=$nextPage;
-				}
-				if($nextPage<=$lastPageNumber)
-				{
-					$helper->saveMemory($pageByParam,$pageId);
-				}
-				$collection->setCurPage($pageByParam);
-				Mage::getSingleton('checkout/session')->setData('pageLoaded',$pageByParam);
-			}
-		}
-		return $this;
 	}
 	
 	public function restoreCollection($observer)
 	{
-		// helper:
-		$helper = Mage::helper('infinitescroll2');
-		// observer data:
-		$event = $observer->getEvent();
-        $collection = $event->getCollection();
-		$lastPageNumber = $collection->getLastPageNumber();
-		if(Mage::registry('current_category') && $helper->isMemoryActive() && $lastPageNumber>1)
-		{
-			// info:
-			$pageLoaded = Mage::getSingleton('checkout/session')->getData('pageLoaded');
-			$nextPageSaved = Mage::getSingleton('checkout/session')->getData('nextPage');
-			$tmpNext = false;
-			// restore page number:
-			$restorePageSize = Mage::getSingleton('checkout/session')->getData('defautlPageSize');
-			$collection->setPageSize($restorePageSize);
-			Mage::getSingleton('checkout/session')->setData('recursiveCollection',false);
-			// last page:
-			$lastPageNumber = $collection->getLastPageNumber();
-			// actions:
-			if(Mage::getSingleton('checkout/session')->getData('recursiveCollection'))
-			{
-				if($pageLoaded>1)
-				{
-					$tmpNext=$pageLoaded+1;
-					if($tmpNext<=$lastPageNumber)
-					{
-						Mage::getSingleton('checkout/session')->setData('nextPage',$tmpNext);
-					}
-					$collection->setCurPage($pageLoaded);
-				}
-			}
-			if(!$tmpNext)
-			{
-				$tmpNext=$pageLoaded+1;
-			}
-			if($helper->isScrollCall() && $nextPageSaved>$lastPageNumber)
-			{
-				die();
-			}
-			if($helper->isScrollCall() && $pageLoaded>1 && $pageLoaded<=$lastPageNumber)
-			{
-				Mage::getSingleton('checkout/session')->setData('nextPage',$tmpNext);
-			}
-		}
-		return $this;
+        // check general and instance enable:
+        $whereare = $this->_whereAreWe();
+        if(Mage::getStoreConfig('infinitescroll2/general/enable') && Mage::getStoreConfig('infinitescroll2/instances/'.$whereare))
+        {
+		    // helper:
+		    $helper = Mage::helper('infinitescroll2');
+		    // observer data:
+		    $event = $observer->getEvent();
+            $collection = $event->getCollection();
+		    $lastPageNumber = $collection->getLastPageNumber();
+		    if(Mage::registry('current_category') && $helper->isMemoryActive() && $lastPageNumber>1)
+		    {
+			    // info:
+			    $pageLoaded = Mage::getSingleton('checkout/session')->getData('pageLoaded');
+			    $nextPageSaved = Mage::getSingleton('checkout/session')->getData('nextPage');
+			    $tmpNext = false;
+			    // restore page number:
+			    $restorePageSize = Mage::getSingleton('checkout/session')->getData('defautlPageSize');
+			    $collection->setPageSize($restorePageSize);
+			    Mage::getSingleton('checkout/session')->setData('recursiveCollection',false);
+			    // last page:
+			    $lastPageNumber = $collection->getLastPageNumber();
+			    // actions:
+			    if(Mage::getSingleton('checkout/session')->getData('recursiveCollection'))
+			    {
+				    if($pageLoaded>1)
+				    {
+					    $tmpNext=$pageLoaded+1;
+					    if($tmpNext<=$lastPageNumber)
+					    {
+						    Mage::getSingleton('checkout/session')->setData('nextPage',$tmpNext);
+					    }
+					    $collection->setCurPage($pageLoaded);
+				    }
+			    }
+			    if(!$tmpNext)
+			    {
+				    $tmpNext=$pageLoaded+1;
+			    }
+			    if($helper->isScrollCall() && $nextPageSaved>$lastPageNumber)
+			    {
+				    die();
+			    }
+			    if($helper->isScrollCall() && $pageLoaded>1 && $pageLoaded<=$lastPageNumber)
+			    {
+				    Mage::getSingleton('checkout/session')->setData('nextPage',$tmpNext);
+			    }
+		    }
+		    return $this;
+        }		    
 	}
 	
 	public function hardReset()
@@ -154,6 +168,15 @@ class Strategery_Infinitescroll2_Model_Catalog_Observer
         }
 	}
 
+    protected function _whereAreWe()
+    {
+        if (Mage::registry('current_category')) { $where = "grid"; }
+        if (Mage::registry('current_category')->getIsAnchor()){ $where = "layer"; }
+        if (Mage::app()->getRequest()->getControllerName() == "result"){ $where = "search"; }
+        if (Mage::app()->getRequest()->getControllerName() == "advanced") { $where = "advanced"; }
+        return $where;
+    }
+    
     protected function _getCache ($observer, $categoryId)
     {
         $collection = $observer->getCollection();
