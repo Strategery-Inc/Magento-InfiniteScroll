172b90409d1bfc557206c568f966cbf8e8977d55
Jan Myszkier <jan.myszkier@strategery.io>
2015-09-10 13:08:26 +0200

    adding an event to hook into once IS is loaded

--- skin/frontend/base/default/js/infinitescroll/ias_config.js
+++ skin/frontend/base/default/js/infinitescroll/ias_config.js
@@ -1,3 +1,8 @@
 window.ias_config = {
     // options from https://github.com/webcreate/infinite-ajax-scroll
 };
+
+/* IS Custom Event to Hook into */
+/*jQuery(document).on('infiniteScrollReady',function(){
+ console.log('bam!');
+ });*/
