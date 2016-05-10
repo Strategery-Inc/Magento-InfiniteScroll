f641fd4cb32164ffe66aca2f8ea13ad2ed0d4a0a
Damian A. Pastorini <damian.pastorini@usestrategery.com>
2016-05-10 17:42:08 -0300

    - Strategery InfiniteScroll 2 - Magento 2

---
+++ app/code/Strategery/Infinitescroll2/view/frontend/web/js/ias_config.js
@@ -0,0 +1,25 @@
+/**
+ * Strategery InfiniteScroll2 - Magento 2 Module
+ *
+ * NOTICE OF LICENSE
+ *
+ * This source file is subject to the Academic Free License (AFL 3.0),
+ * available through the world-wide-web at this URL:
+ * http://opensource.org/licenses/afl-3.0.php
+ *
+ * @license    http://opensource.org/licenses/afl-3.0.php  Academic Free License (AFL 3.0)
+ * @copyright  Copyright (c) 2016 Strategery Inc. (http://www.strategery.io/)
+ * @author     Damian A. Pastorini (damian.pastorini@dwdeveloper.com)
+ */
+
+// IAS config:
+window.ias_config = {
+    // options from https://github.com/webcreate/infinite-ajax-scroll
+};
+
+// IS Custom Event to Hook into:
+/*
+jQuery(document).on('infiniteScrollReady', function(){
+    console.log('bam!');
+});
+*/
